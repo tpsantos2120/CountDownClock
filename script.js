61215f7891b5e29f9aa0aaae50641adca69c2412
LRomero2 <lauren.romero@outlook.com>
2021-06-02 13:26:39 +0000

    Gave each date variavle a unique name as I will now try a different approach.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $("#trigger").click(function(){
   var holidayDates = new Date();
-  var date = new Date(2021,12,25)
-  var date = new Date(2022,01,01)
-  var date = new Date(2022,02,14)
+  var christmas = new Date(2021,12,25)
+  var newYears = new Date(2022,01,01)
+  var valentines = new Date(2022,02,14)
   var calcDaysUntil = parseInt ((date-holidayDates)/(24*3600*1000));
 
   $("#days-until").append("<li>" + calcDaysUntil.toString()+" days until Christmas</li>");
