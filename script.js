d9f1247a51fab13a36134945ff89f157d550b9c6
LRomero2 <lauren.romero@outlook.com>
2021-06-02 16:18:30 +0000

    Made progress and now all code is showing well since I have refactored the code, however days are stil incorrect.

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 $("#trigger").click(function(){
-  var holidayDates = new Date();
-  var christmas = new Date(2021,12,25)
-  var newYears = new Date(2022,01,01)
-  var valentines = new Date(2022,02,14)
-  var calcDaysUntil = parseInt ((date-holidayDates)/(24*3600*1000));
+const holidayDates = [
+[new Date(2021,12,25), ('days until Christmas Day')], 
+[new Date(2022,01,01), ('days until New Years Day')], 
+[new Date(2021,02,14), ('days until Valentines Day')]];
 
-  $("#days-until").append("<li>" + calcDaysUntil.toString()+" days until Christmas</li>");
-  $("#days-until").append("<li>" + calcDaysUntil.toString()+" days until New Year</li>");
-  $("#days-until").append("<li>" + calcDaysUntil.toString()+" days until Valentines Day</li>");
+holidayDates.forEach(item => { 
+    const calcDaysUntil = parseInt ((item[0] - new Date()) / (24*60*60*1000));
+    const countdownResult = "<li>" + calcDaysUntil + " days until " + item[1] + "</li>";
+$("#days-until").append(countdownResult);
 })
-
-
-$("#trigger").click(function(){
-const holidayDates = [[new Date(2021,12,25), 'days until Christmas Day'], [new Date(2022,01,01), 'days until New Years Day'], 
-[new Date(2021,02,14), 'days until Valentines Day']];
-
-holidayDates.forEach(item => { var calcDaysUntil = parseInt (24*3600*1000));
 })
 
