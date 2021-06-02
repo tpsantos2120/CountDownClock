8ffdef53c5725525a257d23d1c7e96af6e745eb7
LRomero2 <lauren.romero@outlook.com>
2021-06-02 22:03:14 +0000

    Now happy with the code but still lacking the correct numbers in the executed code.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+
+
 $("#trigger").click(function(){
 const holidayDates = [
 [new Date(2021,12,25), ('days until Christmas Day')], 
 [new Date(2022,01,01), ('days until New Years Day')], 
 [new Date(2021,02,14), ('days until Valentines Day')]];
 
+var today = new Date(2021,06,02).getTime();
+
+
 holidayDates.forEach(item => { 
-    const calcDaysUntil = parseInt ((item[0] - new Date()) / (24*60*60*1000));
-    const countdownResult = "<li>" + calcDaysUntil + " days until " + item[1] + "</li>";
+    const calcDaysUntil = parseInt ((item[0] - new Date()) / (24 * 60 * 60 * 1000));
+    const countdownResult = "<li>" + calcDaysUntil + " days-until " + item[1] + "</li>";
 $("#days-until").append(countdownResult);
 })
 })
