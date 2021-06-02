265dde229368ca13433311196ae528fc6b1f1e27
LRomero2 <lauren.romero@outlook.com>
2021-06-02 12:03:54 +0000

    COde working apart from displaying the correct individual days for the dates.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 $("#trigger").click(function(){
-  var HolidayDates = new Date();
-  var Christmas = new Date(2021,12,25)
-  var NewYear = new Date(2022,01,01)
-  var Valentines = new Date(2022,02,14)
-  var CalcDaysUntil = parseInt((Valentines-HolidayDates)/(24*3600*1000));
-  
-  $("#days-until").append("<li>" + CalcDaysUntil.toString()+" days until Christmas</li>");
-  $("#days-until").append("<li>" + CalcDaysUntil.toString()+" days until New Year</li>");
-  $("#days-until").append("<li>" + CalcDaysUntil.toString()+" days until Valentines Day</li>");
+  var holidayDates = new Date();
+  var date = new Date(2021,12,25)
+  var date = new Date(2022,01,01)
+  var date = new Date(2022,02,14)
+  var calcDaysUntil = parseInt ((date-holidayDates)/(24*3600*1000));
+
+  $("#days-until").append("<li>" + calcDaysUntil.toString()+" days until Christmas</li>");
+  $("#days-until").append("<li>" + calcDaysUntil.toString()+" days until New Year</li>");
+  $("#days-until").append("<li>" + calcDaysUntil.toString()+" days until Valentines Day</li>");
 })
 
