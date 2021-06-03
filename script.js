ab5220842f919429bd927bc411783e35774ba000
LRomero2 <lauren.romero@outlook.com>
2021-06-03 08:07:32 +0000

    Changed some code for the math to get the current time and time of holiday dates. Also changed the countdown result to jquery.

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
+$("#trigger").click(function () {
+  const holidayDates = [
+    [new Date(2021,12,25), ('days until Christmas Day')], 
+    [new Date(2022,01,01), ('days until New Years Day')], 
+    [new Date(2021,02,14), ('days until Valentines Day')]];
 
-
-$("#trigger").click(function(){
-const holidayDates = [
-[new Date(2021,12,25), ('days until Christmas Day')], 
-[new Date(2022,01,01), ('days until New Years Day')], 
-[new Date(2021,02,14), ('days until Valentines Day')]];
-
-var today = new Date(2021,06,02).getTime();
-
-
-holidayDates.forEach(item => { 
-    const calcDaysUntil = parseInt ((item[0] - new Date()) / (24 * 60 * 60 * 1000));
-    const countdownResult = "<li>" + calcDaysUntil + " days-until " + item[1] + "</li>";
-$("#days-until").append(countdownResult);
-})
-})
+  const today = new Date();
+  
+  holidayDates.forEach((item) => 
+  {const calcDaysUntil = Math.ceil((item[0].getTime() - today.getTime()) / (1000 * 60 * 60 * 24));
+    console.log(item)
+   
+    const countdownResult = `<li> ${calcDaysUntil} ${item[1]}</li>`;
+    $("#days-until").append(countdownResult);
+  });
+});
 
